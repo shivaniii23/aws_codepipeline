aee9482d8f4a27fb20f83a5eb24425a274d7e54d
Ubuntu <ubuntu@ip-172-31-81-3.ec2.internal>
2024-07-13 12:26:51 +0000

    add files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull shivanij2304/python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 shivanij2304/python-app:latest
+docker run -d -p 5000:5000 --name python-app shivanij2304/python-app:latest
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Stop the running container (if any)
-docker stop shivanij2304/python-app
+docker stop python-app

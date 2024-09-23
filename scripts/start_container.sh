50bd7301d7972c6e6c8bcb12bd4dab876c6d8ebf
shivaniii23 <54891990+shivaniii23@users.noreply.github.com>
2024-09-23 15:35:28 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull shivanij2304/python-app
+docker pull shivanij2304/python
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 --name python-app shivanij2304/python-app:latest
+docker run -d -p 5000:5000 --name python-app shivanij2304/python:latest
